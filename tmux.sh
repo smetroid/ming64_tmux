404ce99d4471a8a120be2f48da881b37c77b597a
EC <enriquegc1982@gmail.com>
2017-10-29 20:51:11 -0500

    adding tmux.sh to download and install pre-requsites and tmux from source

---
+++ tmux.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Script for installing tmux on systems where you don't have root access.
+# tmux will be installed in $HOME/local/bin.
+# It's assumed that wget and a C/C++ compiler are installed.
+
+# exit on error
+set -e
+
+TMUX_VERSION=2.6
+
+# create our directories
+mkdir -p $HOME/local $HOME/tmux_tmp
+cd $HOME/tmux_tmp
+
+# download source files for tmux, libevent, and ncurses
+#wget -O tmux-${TMUX_VERSION}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz/download
+wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz
+
+# extract files, configure, and compile
+
+############
+# libevent #
+############
+tar xvzf libevent-2.1.8-stable.tar.gz
+cd libevent-2.1.8-stable
+./configure --host=x86_64-w64-mingw32 --prefix=$HOME/local --disable-shared
+make
+make install
+cd ..
+
+############
+# ncurses  #
+############
+tar xvzf ncurses-6.0.tar.gz
+cd ncurses-6.0
+./configure --host=x86_64-w64-mingw32 --with-build-cc --prefix=$HOME/local --enable-term-driver --enable-sp-funcs
+make
+make install
+cd ..
+
+############
+# tmux     #
+############
+tar xvzf tmux-${TMUX_VERSION}.tar.gz
+cd tmux-${TMUX_VERSION}
+./configure --host=x86_64-w64-mingw32 CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include" CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" 
+make
+cp tmux $HOME/local/bin
+cd ..
+
+# cleanup
+rm -rf $HOME/tmux_tmp
+
+echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
